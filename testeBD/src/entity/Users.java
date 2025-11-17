bf2ec57d9c0687d006c084babf8be9a16ffa7887
Pessinipp <gabrielpessini2006@gmail.com>
2025-11-17 20:26:29 -0300

    Refatorando users.java

--- testeBD/src/entity/Users.java
+++ testeBD/src/entity/Users.java
@@ -1,8 +1,15 @@
 package entity;
 
+import java.util.Vector;
+
 public class Users {
     public int id;
     public String username;
     public String password;
 
+        // Método para converter um objeto Users em um array de objetos
+        public Object[] toArray() {
+            return new Object[]{id, username};
+        }
 }
+
