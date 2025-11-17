b5d19287b58b6757cdc62008bf47685e2c12481f
Pessinipp <gabrielpessini2006@gmail.com>
2025-11-17 20:22:16 -0300

    Refatorando Task.java

--- testeBD/src/entity/Task.java
+++ testeBD/src/entity/Task.java
@@ -5,4 +5,9 @@ public class Task {
     public int user_id;
     public String description;
     public String status;
+
+            // Método para converter um objeto Users em um array de objetos
+            public Object[] toArray() {
+                return new Object[]{id, user_id, description,status};
+            }
 }
