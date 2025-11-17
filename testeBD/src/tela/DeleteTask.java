29cb704cc59ab35e09d0d1cb90061e24b0f5aaaa
Gutinm <78541653+Gutinm@users.noreply.github.com>
2025-11-17 20:38:45 -0300

    atualização tela delete task

--- testeBD/src/tela/DeleteTask.java
+++ testeBD/src/tela/DeleteTask.java
@@ -7,7 +7,6 @@ import DAO.*;
 
 public class DeleteTask extends JFrame implements ActionListener {
     private JTextField campoUsuario;
-    private JPasswordField campoSenha;
     private JButton botaoLogin;
     UsersDAO usersDAO = new UsersDAO();
     TaskDAO taskDAO = new TaskDAO(); 
@@ -25,7 +24,19 @@ public class DeleteTask extends JFrame implements ActionListener {
         botaoLogin = new JButton("Deletar");
 
         botaoLogin.addActionListener(this);
-        
+
+        // Criando e adicionando o botão "Voltar"
+        JButton btnVoltar = new JButton("Voltar");
+        btnVoltar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Fecha a janela quando o botão "Voltar" é clicado
+                dispose();
+                new Menu();
+            }
+        });
+        getContentPane().add(btnVoltar, BorderLayout.SOUTH);
+
         painel.add(labelUsuario);
         painel.add(campoUsuario);
         painel.add(new JLabel()); // Espaço vazio
@@ -34,6 +45,8 @@ public class DeleteTask extends JFrame implements ActionListener {
         add(painel);
 
         setVisible(true);
+
+        
     }
 
     @Override
